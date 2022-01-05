730071459057982ba7ab3ec85e9c3bfd0f52688f
alexandret <alexandrettio@gmail.com>
2022-01-05 13:18:42 +0300

    added model profile mvp

--- chelovekobook/urls.py
+++ chelovekobook/urls.py
@@ -1,8 +1,8 @@
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
 from people import views
-
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -13,5 +13,6 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- people/models.py
+++ people/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import TextField, CharField, ImageField
+
+
+class Profile(models.Model):
+    full_name = CharField(max_length=100)
+    photo = ImageField()
+    about = TextField()
